df59ea9394693c04f8e919be96811c5602cef8bc
hrustinszkiadam <hrustinszkiadam@gmail.com>
2024-12-02 16:15:03 +0100

    base classes

---
+++ Jarat.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JaratKezeloProject
+{
+	public class Jarat
+	{
+		public string jaratSzam { get; set; }
+		public string honnanRepter { get; set; }
+		public string hovaRepter { get; set; }
+		public DateTime indulas { get; set; }
+		public TimeSpan keses { get; set; }
+
+		public Jarat(string jaratSzam, string honnanRepter, string hovaRepter, DateTime indulas)
+		{
+			if (string.IsNullOrEmpty(jaratSzam)) throw new ArgumentNullException("A járat szám nem lehet üres!", nameof(jaratSzam));
+			if (string.IsNullOrEmpty(honnanRepter)) throw new ArgumentNullException("A járat szám nem lehet üres!", nameof(honnanRepter));
+			if (string.IsNullOrEmpty(hovaRepter)) throw new ArgumentNullException("A járat szám nem lehet üres!", nameof(hovaRepter));
+
+			this.jaratSzam = jaratSzam;
+			this.honnanRepter = honnanRepter;
+			this.hovaRepter = hovaRepter;
+			this.indulas = indulas;
+			this.keses = TimeSpan.Zero;
+		}
+	}
+}
---
+++ JaratKezelo.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JaratKezeloProject
+{
+	public class JaratKezelo
+	{
+		private List<Jarat> jaratok;
+
+		public JaratKezelo()
+		{
+			jaratok = new List<Jarat>();
+		}
+
+		public void UjJarat(string jaratSzam, string repterHonnan, string repterHova, DateTime indulas)
+		{
+			Jarat ujJarat = new Jarat(jaratSzam, repterHonnan, repterHova, indulas);
+
+			if (jaratok.Exists(j => j.jaratSzam == jaratSzam)) throw new ArgumentException("A járat már létezik!", nameof(ujJarat));
+
+			jaratok.Add(ujJarat);
+		}
+
+		public void Keses(string jaratSzam, TimeSpan keses)
+		{
+			if (string.IsNullOrEmpty(jaratSzam)) throw new ArgumentNullException("A járat szám nem lehet üres!", nameof(jaratSzam));
+
+			var jarat = jaratok.Find(j => j.jaratSzam == jaratSzam);
+			if (jarat == null) throw new ArgumentException("Nincs ilyen járat!", nameof(jaratSzam));
+
+			if (jarat.keses + keses < TimeSpan.Zero) throw new NegativKesesException();
+
+
+			jarat.keses += keses;
+		}
+
+		public DateTime MikorIndul(string jaratSzam)
+		{
+			if (string.IsNullOrEmpty(jaratSzam)) throw new ArgumentNullException("A járat szám nem lehet üres!", nameof(jaratSzam));
+			var jarat = jaratok.Find(j => j.jaratSzam == jaratSzam);
+			if (jarat == null) throw new ArgumentException("Nincs ilyen járat!", nameof(jaratSzam));
+
+
+			return jarat.indulas + jarat.keses;
+		}
+
+		public List<string> JaratokRepuloterrol(string repter)
+		{
+			if (string.IsNullOrEmpty(repter)) throw new ArgumentNullException("A reptér nem lehet üres", nameof(repter));
+
+			return jaratok.FindAll(j => j.honnanRepter == repter).Select(j => j.jaratSzam).ToList();
+		}
+
+	}
+}
